27c4ef85741e76ad936adfb8cecce8756b9182be
Syksy <teemu.daniel.laajala@gmail.com>
2024-05-10 09:56:25 +0300

    Better regex subs for LLama

--- funcs.py
+++ funcs.py
@@ -2,10 +2,15 @@ import re
 
 # LLama does not fully adhere to the JSON standard, and requires additional parsing/sanitization to be JSON-readable
 def jsonSanitize(text):
-    return re.sub(": 3\+3", ": \"3+3\"", re.sub(": 3\+4", ": \"3+4\"", re.sub(": 4\+3", ": \"4+3\"",
+    return (
+        # Sometimes number+number returned without quotes in LLama; fix few most obvious cases
+        re.sub(": 3\+3\n", ": \"3+3\"\n", re.sub(": 3\+4\n", ": \"3+4\"\n", re.sub(": 4\+3\n", ": \"4+3\"\n",
+        re.sub(": 3\+3,", ": \"3+3\",", re.sub(": 3\+4,", ": \"3+4\",", re.sub(": 4\+3,", ": \"4+3\",",
         # Some convenience subs
         re.sub(": NA", ": \"NA\"", re.sub("\)\n", "\n", re.sub("\),",",", re.sub(": \(",": ",
             # Native json.loads cannot handle //-comments at the end of lines in JSON files (?)
-            re.sub("\/\/.*","", text)))))
-        )))
-
+            re.sub("\/\/.*","",
+                   text)
+            ))))
+        ))))))
+    )
